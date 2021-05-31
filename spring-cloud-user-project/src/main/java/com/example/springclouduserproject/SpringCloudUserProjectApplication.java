7603c09ff884da425cff1b604cc25d5da88d2ca7
qrn <miraitowa1796@163.com>
2021-05-31 20:38:32 +0800

    SpringCloud 第一次提交

---
+++ spring-cloud-user-project/src/main/java/com/example/springclouduserproject/SpringCloudUserProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springclouduserproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserProjectApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-user-project/src/test/java/com/example/springclouduserproject/SpringCloudUserProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springclouduserproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudUserProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
